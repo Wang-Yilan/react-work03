b89f9f3eda23b2b7b6d5391db1aa5de157210415
Wang-Yilan <1354012000@qq.com>
2020-10-25 18:16:17 +0800

    Work03

---
+++ build/precache-manifest.5b89eb339267d6d03091cbdcdb5d5ffb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd0f854373b091a5f9965e3d3f007cd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3603d58a6e42543b4742",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "f2072fdca16e0a0f2f62",
+    "url": "/static/css/main.a7dadf2c.chunk.css"
+  },
+  {
+    "revision": "3603d58a6e42543b4742",
+    "url": "/static/js/2.b1c1f6b2.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.b1c1f6b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2072fdca16e0a0f2f62",
+    "url": "/static/js/main.2672938f.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "6b55396ec634fbe26567a34c9751831f",
+    "url": "/static/media/taobao.6b55396e.PNG"
+  }
+]);
